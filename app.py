c0f55e9c8b8ae07e58a7db2c4591fb4a550e773c
951237 <951237.gmail.com>
2020-02-08 01:38:14 +0900

    Doc : Create - 플라스크 따라하기 - 레이아웃을 이용하여 코드블럭으로 간단히 편집하기 - 부트스트랩 적용

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template    
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+posts = [
+    {
+        '작성자' : '951237',
+        '제목' : '블로그 포스트 1',
+        '내용': '첫번째 포스트',
+        '작성일': '2020. 2. 7',
+    },
+    {
+        '작성자': 'cutemin226',
+        '제목': '블로그 포스트 2',
+        '내용': '두번째 포스트',
+        '작성일': '2020. 2. 8',
+    },
+]
+
 @app.route('/')
 @app.route('/home')     # home 페이지
 def home():
-    return '<h1>Home page</h1>'
+    return render_template('home.html', posts = posts) # templates폴더의  파일
+    # 불러오기 post는 페이지의 내용
 
-@app.route('/about')    # about 페이
+@app.route('/about')    # about 페이지 
 def about():
-    return '<h1>About page</h1>'
+    return render_template('about.html', title = 'About') # templates폴더의 파일
+    # 불러오기 'title'는 웹페이지 윈도우 이름
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
