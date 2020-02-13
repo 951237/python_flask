2e6155a2278df533e9ee1cb989a05ec1b7d75298
951237 <951237.gmail.com>
2020-02-14 00:45:20 +0900

    Docs : Update - 텐서플로 2.0업그레이드로 인하여 placeholder 메소드가 작동하지 않음. - 버전 1을 버전2의 방식으로 바꿔주도록 임포트를 했으나 되지 않음. - 코드는 주석처리해놓음. - 해결책 : 텐서플로를 1버전으로 설치하여 완성하기!

--- TIL - AI_Web_app_200211/server.py
+++ TIL - AI_Web_app_200211/server.py
@@ -2,20 +2,22 @@
 
 from flask import Flask, render_template, request  # 라이브러리 호출
 
-# import datetime
-# import tensorflow as tf
-# import numpy as np
+import datetime
+import tensorflow as tf
+# import tensorflow.compat.v1 as tf
+# tf.disable_v2_behavior()
+import numpy as np
 
 app = Flask(__name__)  # 플라스크 앱 설정
 
-# x = tf.placeholder(tf.float32, shape = [None, 4])
+# x = tf.placeholder(tf.float32, shape=[None, 4])
 # y = tf.placeholder(tf.float32, shape=[None, 1])
-# w = tf.Variable(tf.random_normal([4,1], name = 'weight'))
-# b = tf.Variable(tf.random_normal([1]), name = 'bias')
+# w = tf.Variable(tf.random_normal([4, 1], name='weight'))
+# b = tf.Variable(tf.random_normal([1]), name='bias')
 #
 # hypothesis = tf.matmul(x, y) + b
 #
-# saver= tf.train.Saver()
+# saver = tf.train.Saver()
 # model = tf.global_variables_initializer()
 #
 # sess = tf.Session()
@@ -24,12 +26,13 @@ app = Flask(__name__)  # 플라스크 앱 설정
 # save_path = './model/saved.cpkt'
 # saver.restore(sess, save_path)
 
+
 @app.route("/", methods=['GET', 'POST'])  # 겟방식과 포스트 방식을 모두 선언
 def index():  # 웹페이지 파일의 이름과 동일
     return render_template('index.html')
-    # if request.method == 'GET'
+    # if request.method == 'GET':
     #     return render_template('index.html')
-    # if request.method =='POST':
+    # if request.method == 'POST':
     #     avg_temp = float(request.form['avg_temp'])
     #     min_temp = float(request.form['min_temp'])
     #     max_temp = float(request.form['max_temp'])
@@ -38,12 +41,12 @@ def index():  # 웹페이지 파일의 이름과 동일
     # price = 0
     #
     # data = ((avg_temp, min_temp, max_temp, rain_fall),)
-    # arr = np.array(data, dtype = np.float32)
+    # arr = np.array(data, dtype=np.float32)
     #
     # x_data = arr[0:4]
-    # dict = sess.run(hypothesis, feed_fict = {x : x_data})
+    # dict = sess.run(hypothesis, feed_fict={x: x_data})
     # print(dict[0])
-    # return render_template('index.html', price = price)
+    # return render_template('index.html', price=price)
 
 
 if __name__ == '__main__':
