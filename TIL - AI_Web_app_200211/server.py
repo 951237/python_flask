86aab3d5def44d296cd41624a90afcc491afdf9f
951237 <951237@gmail.com>
2020-05-09 01:58:39 +0900

    Doc : Update - 모델을 플라스크에 이식했으나 html로 출력되지 않음.

--- TIL - AI_Web_app_200211/server.py
+++ TIL - AI_Web_app_200211/server.py
@@ -1,50 +1,35 @@
 # -*- coding : utf-8 -*-
 
-from flask import Flask, render_template  # 라이브러리 호출
+from flask import Flask, render_template, request  # 라이브러리 호출
 
-# import tensorflow.compat.v1 as tf
-# tf.disable_v2_behavior()
+import numpy as np
+from tensorflow import keras
+import tensorflow as tf
+
 
 app = Flask(__name__)  # 플라스크 앱 설정
 
-
-# x = tf.placeholder(tf.float32, shape=[None, 4])
-# y = tf.placeholder(tf.float32, shape=[None, 1])
-# w = tf.Variable(tf.random_normal([4, 1], name='weight'))
-# b = tf.Variable(tf.random_normal([1]), name='bias')
-#
-# hypothesis = tf.matmul(x, y) + b
-#
-# saver = tf.train.Saver()
-# model = tf.global_variables_initializer()
-#
-# sess = tf.Session()
-# sess.run(model)
-#
-# save_path = './model/saved.cpkt'
-# saver.restore(sess, save_path)
+# 가중치와 옵티마이저를 포함하여 정확히 동일한 모델을 다시 생성합니다
+new_model = keras.models.load_model('./model/predic_cabbage.h5')
 
 
 @app.route("/", methods=['GET', 'POST'])  # 겟방식과 포스트 방식을 모두 선언
 def index():  # 웹페이지 파일의 이름과 동일
-    return render_template('index.html')
-    # if request.method == 'GET':
-    #     return render_template('index.html')
-    # if request.method == 'POST':
-    #     avg_temp = float(request.form['avg_temp'])
-    #     min_temp = float(request.form['min_temp'])
-    #     max_temp = float(request.form['max_temp'])
-    #     rain_fall = float(request.form['rain_fall'])
-    #
-    # price = 0
-    #
-    # data = ((avg_temp, min_temp, max_temp, rain_fall),)
-    # arr = np.array(data, dtype=np.float32)
-    #
-    # x_data = arr[0:4]
-    # dict = sess.run(hypothesis, feed_fict={x: x_data})
-    # print(dict[0])
-    # return render_template('index.html', price=price)
+    if request.method == 'GET':
+        return render_template('index.html')
+
+    if request.method == 'POST':
+        avg_temp = float(request.form['avg_temp'])
+        min_temp = float(request.form['min_temp'])
+        max_temp = float(request.form['max_temp'])
+        rain_fall = float(request.form['rain_fall'])
+
+    price = 0
+
+    data = [avg_temp, min_temp, max_temp, rain_fall]
+    dic = new_model.predict(np.array(data).reshape(1, 4))
+    price = dic[0]
+    return render_template('index.html', price = price)
 
 
 if __name__ == '__main__':
