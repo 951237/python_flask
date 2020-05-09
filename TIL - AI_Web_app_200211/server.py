71a2bd7786251af73c22a36540d3855d7e739744
951237 <951237@gmail.com>
2020-05-10 01:00:28 +0900

    Doc : Update

--- TIL - AI_Web_app_200211/server.py
+++ TIL - AI_Web_app_200211/server.py
@@ -2,19 +2,24 @@
 
 from flask import Flask, render_template, request  # 라이브러리 호출
 
-import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from tensorflow import keras
-import tensorflow as tf
 
+import numpy as np
 
 app = Flask(__name__)  # 플라스크 앱 설정
 
-# 가중치와 옵티마이저를 포함하여 정확히 동일한 모델을 다시 생성합니다
+model = Sequential()
+model.add(Dense(1, input_dim=4))
+
 new_model = keras.models.load_model('./model/predic_cabbage.h5')
 
 
 @app.route("/", methods=['GET', 'POST'])  # 겟방식과 포스트 방식을 모두 선언
 def index():  # 웹페이지 파일의 이름과 동일
+    # 가중치와 옵티마이저를 포함하여 정확히 동일한 모델을 다시 생성합니다
+
     if request.method == 'GET':
         return render_template('index.html')
 
@@ -29,7 +34,7 @@ def index():  # 웹페이지 파일의 이름과 동일
     data = [avg_temp, min_temp, max_temp, rain_fall]
     dic = new_model.predict(np.array(data).reshape(1, 4))
     price = dic[0]
-    return render_template('index.html', price = price)
+    return render_template('index.html', price=price)
 
 
 if __name__ == '__main__':
