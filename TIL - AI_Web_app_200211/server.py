8c38088c6d9b38f4b9b945c756b8a13597e32b5e
jinil <951237@gmail.com>
2020-05-13 17:33:06 +0900

    Doc : Update - 파이선 파일에서 html에 데이터 표현 가능

--- TIL - AI_Web_app_200211/server.py
+++ TIL - AI_Web_app_200211/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)  # 플라스크 앱 설정
 
 @app.route("/", methods=['GET', 'POST'])  # 겟방식과 포스트 방식을 모두 선언
 def index():  # 웹페이지 파일의 이름과 동일
-    return render_template('index.html', price = 0)
+    return render_template('index_new.html', price='0')
 
 
 if __name__ == '__main__':
