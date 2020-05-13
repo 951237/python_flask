84514a52c5e4cb81fcc7f6c59b081b3caa880c23
951237 <951237@gmail.com>
2020-05-14 00:49:26 +0900

    Doc : Update - 예측결과 웹페이지 구현 성공!

--- TIL - AI_Web_app_200211/server.py
+++ TIL - AI_Web_app_200211/server.py
@@ -1,15 +1,37 @@
 # -*- coding : utf-8 -*-
 
 from flask import Flask, render_template, request  # 라이브러리 호출
-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow import keras
+import numpy as np
 
 app = Flask(__name__)  # 플라스크 앱 설정
+
+model = Sequential()
+model.add(Dense(1, input_dim=4))
+
+new_model = keras.models.load_model('./model/predic_cabbage.h5')
 
 
 @app.route("/", methods=['GET', 'POST'])  # 겟방식과 포스트 방식을 모두 선언
 def index():  # 웹페이지 파일의 이름과 동일
-    return render_template('index_new.html', price='0')
+    if request.method == 'GET':
+        return render_template('index_new.html')
+
+    if request.method == 'POST':
+        avg_temp = float(request.form['avg_temp'])
+        min_temp = float(request.form['min_temp'])
+        max_temp = float(request.form['max_temp'])
+        rain_fall = float(request.form['rain_fall'])
+
+        price = 0
+
+        data = [avg_temp, min_temp, max_temp, rain_fall]
+        dic = new_model.predict(np.array(data).reshape(1, 4))
+
+        return render_template('index_new.html', price=dic)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
--- TIL - AI_Web_app_200211/server_old.py
+++ TIL - AI_Web_app_200211/server_old.py
@@ -2,18 +2,18 @@
 
 from flask import Flask, render_template, request  # 라이브러리 호출
 
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense
-# from tensorflow import keras
-#
-# import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow import keras
+
+import numpy as np
 
 app = Flask(__name__)  # 플라스크 앱 설정
 
-# model = Sequential()
-# model.add(Dense(1, input_dim=4))
-#
-# new_model = keras.models.load_model('./model/predic_cabbage.h5')
+model = Sequential()
+model.add(Dense(1, input_dim=4))
+
+new_model = keras.models.load_model('./model/predic_cabbage.h5')
 
 
 @app.route("/", methods=['GET', 'POST'])  # 겟방식과 포스트 방식을 모두 선언
@@ -29,11 +29,11 @@ def index():  # 웹페이지 파일의 이름과 동일
         max_temp = float(request.form['max_temp'])
         rain_fall = float(request.form['rain_fall'])
     #
-    # price = 0
-    #
-    # # data = [avg_temp, min_temp, max_temp, rain_fall]
-    # # dic = new_model.predict(np.array(data).reshape(1, 4))
-    # price = avg_temp + min_temp + max_temp + rain_fall
+    price = 0
+
+    # data = [avg_temp, min_temp, max_temp, rain_fall]
+    dic = new_model.predict(np.array(data).reshape(1, 4))
+    price = avg_temp + min_temp + max_temp + rain_fall
     return render_template('index.html', price=avg_temp)
 
 
