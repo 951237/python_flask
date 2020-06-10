005d95c1f291de105d9aa1025c8ba5c9140855b3
951237 <951237@gmail.com>
2020-06-10 23:53:57 +0900

    Doc : Update -  예외처리 - 원하는 지역을 찾지 못했을 경우 예외처리하여 에러 출력 페이지로 이동하기

--- myCity_population/server.py
+++ myCity_population/server.py
@@ -19,37 +19,42 @@ def index():
         return render_template('index.html')
 
     if request.method == 'POST':
-        city = str(request.form['city'])
-        df_city = df[df['행정구역'].str.contains(city)]  # 새솔동 이름 들어간 행을 선택하기
 
-        if len(df_city) > 1:
-            df_city = df_city.iloc[0:1, :]
-        else:
-            pass
+        try:
+            city = str(request.form['city'])
+            df_city = df[df['행정구역'].str.contains(city)]  # 새솔동 이름 들어간 행을 선택하기
 
-        df_man = df_city.iloc[:, 3:104]  # 남자 인구부분만 선택
-        df_woman = df_city.iloc[:, 106:208]  # 여자 인구부분만 선택
-        time.sleep(0.5)
+            if len(df_city) > 1:
+                df_city = df_city.iloc[0:1, :]
+            else:
+                pass
 
-        man = df_man.T  # 데이터 프레임 행렬 바꾸
-        man = man.reset_index(drop=True)  # 인덱스 0부터 번호 다시 따기
-        man.columns = ['man']  # 칼럼 이름 바꾸기
-        woman = df_woman.T  # 데이터 프레임 행렬 바꾸기
-        woman = woman.reset_index(drop=True)  # 인덱스 0부터 번호 다시 따기
-        woman.columns = ['woman']  # 칼럼 이름 바꾸기
-        df_merge = pd.merge(man, woman, how='outer', left_index=True,
-                            right_index=True)  # 데이터 프레임 합치기
-        time.sleep(0.5)
+            df_man = df_city.iloc[:, 3:104]  # 남자 인구부분만 선택
+            df_woman = df_city.iloc[:, 106:208]  # 여자 인구부분만 선택
+            time.sleep(0.5)
 
-        plt.figure(figsize=(14, 7), dpi=150)
-        plt.plot(df_merge)
-        df_merge.plot(figsize=(8, 4))
-        plt.draw()
-        fig = plt.gcf()
-        fig.savefig(f'./static/image/myFile_{city}.png', dpi=fig.dpi)
-        time.sleep(3)
-        # return render_template('result.html', city=city, data=df_merge.to_html())
-        return render_template('result.html', city=city, image_file=f'image/myFile_{city}.png')
+            man = df_man.T  # 데이터 프레임 행렬 바꾸
+            man = man.reset_index(drop=True)  # 인덱스 0부터 번호 다시 따기
+            man.columns = ['man']  # 칼럼 이름 바꾸기
+            woman = df_woman.T  # 데이터 프레임 행렬 바꾸기
+            woman = woman.reset_index(drop=True)  # 인덱스 0부터 번호 다시 따기
+            woman.columns = ['woman']  # 칼럼 이름 바꾸기
+            df_merge = pd.merge(man, woman, how='outer', left_index=True,
+                                right_index=True)  # 데이터 프레임 합치기
+            time.sleep(0.5)
+
+            plt.figure(figsize=(14, 7), dpi=150)
+            plt.plot(df_merge)
+            df_merge.plot(figsize=(8, 4))
+            plt.draw()
+            fig = plt.gcf()
+            fig.savefig(f'./static/image/myFile_{city}.png', dpi=fig.dpi)
+            time.sleep(3)
+            # return render_template('result.html', city=city, data=df_merge.to_html())
+            return render_template('result.html', city=city, image_file=f'image/myFile_{city}.png')
+        
+        except:
+            return render_template('error.html')
 
 
 if __name__ == '__main__':
